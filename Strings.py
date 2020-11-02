fa5a7784d5b63d7e8be33389b2ec131552f89ccd
muhammad743 <72973879+muhammad743@users.noreply.github.com>
2020-11-02 16:59:34 +0500

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,31 @@
+# Today, Well learn about strings!
+***************************************************************
+# A Simple String
+cousre = 'Python for Beginners'
+print(cousre)
+
+# If you want to use an apostrophe in a String;
+cousre = "Python for Beginner's" (# Use double-qoutes.)
+
+# If you want to use double-qoutes to highlight a word in a String;
+cousre = 'Python for Beginners"' (# Use single-qoutes.)
+
+# If you want a multi-line String;
+cousre = """
+Python
+for 
+Beginners
+"""
+***************************************************************
+# String Methods:
+
+#Capitilization;
+.upper()
+#Lower-case;
+.Lower()
+#replace a word;
+.replace('Word to be replaced', 'word of choice')
+# Find the index of a word
+.find('Letter only')
+
+print("Thanks for reading these notes and don't forget to subsctibe to my channel, like and share")
